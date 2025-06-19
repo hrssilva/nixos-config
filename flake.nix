02f9e04ac2ad99b7ff863bf77c3378fc4ea1afc0
hrssilva <haroldo.rssilva@gmail.com>
2025-06-18 21:21:19 -0300

    moved configuration to outside root for better git support

--- flake.nix
+++ flake.nix
@@ -8,12 +8,12 @@
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
-        /etc/nixos/configuration.nix
+        ./configuration.nix
       ];
     };
   };
