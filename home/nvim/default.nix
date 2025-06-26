5d09aa193ab2c8a664c722a2635a6cd7f7fefca9
hrssilva <haroldo.rssilva@gmail.com>
2025-06-25 22:15:03 -0300

    Configured Treesittter with all langs

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -28,13 +28,14 @@ in {
     vim.lsp.enable('pyright')
     vim.lsp.enable('tsserver')
     vim.lsp.enable('marksman')
-    vim.lsp.enable('nil')
+    vim.lsp.enable('nil_ls')
     local builtin = require('telescope.builtin')
     local lspconfig = require("lspconfig")
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope search diagnostics' })
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('my.lsp', {}),
       callback = function(args)
@@ -45,6 +46,7 @@ in {
         if client:supports_method('textDocument/implementation') then
           -- Create a keymap for vim.lsp.buf.implementation ...
             vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to Implementation"))
+        end
         if client:supports_method('textDocument/definition') then
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to Definition"))
         end
@@ -66,6 +68,9 @@ in {
         if client:supports_method('textDocument/codeAction') then
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code Action"))
         end
+        if client:supports_method('textDocument/diagnostic') then
+            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.diagnostic, opts("Code Diagnostics"))
+        end
 
         -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
         if client:supports_method('textDocument/completion') then
@@ -75,6 +80,9 @@ in {
 
           vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
         end
+          vim.keymap.set('i', '<c-space>', function()
+            vim.lsp.completion.get()
+          end)
 
         -- Auto-format ("lint") on save.
         -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
@@ -90,6 +98,18 @@ in {
         end
       end,
     })
+    require'nvim-treesitter.configs'.setup {
+      highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+    }
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     '';
   };
 }
