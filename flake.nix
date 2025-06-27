6b65c91f3369c23c0383fc7a4a84674ae7cab101
hrssilva <haroldo.rssilva@gmail.com>
2025-06-27 00:25:02 -0300

    fixing let...in block at top

--- flake.nix
+++ flake.nix
@@ -1,54 +1,55 @@
-let 
-    passOn = {
-        username = "hrssilva";
-        userdescription = "Haroldo R. S. Silva";
-        usergroups = [];
-        };
-in {
-  description = "A simple NixOS flake";
+{
+    description = "A simple NixOS flake";
 
-  inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # ghostty.url = "github:ghostty-org/ghostty/main";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        # NixOS official package source, using the nixos-25.05 branch here
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        # ghostty.url = "github:ghostty-org/ghostty/main";
+        home-manager = {
+          url = "github:nix-community/home-manager/release-25.05";
+          # The `follows` keyword in inputs is used for inheritance.
+          # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+          # the `inputs.nixpkgs` of the current flake,
+          # to avoid problems caused by different versions of nixpkgs.
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
+
     };
 
-  };
 
 
-
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+    let 
+        passOn = {
+            username = "hrssilva";
+            userdescription = "Haroldo R. S. Silva";
+            usergroups = [];
+            };
+    in {
         # Please replace my-nixos with your hostname
         nixosConfigurations = {
             laptop = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
+                system = "x86_64-linux";
 
-            specialArgs = { inherit inputs; super = self; hostname = "laptop"; } // passOn;
-            modules = [
-            # Import the previous configuration.nix we used,
-            # so the old configuration file still takes effect
-            ./system/hosts/laptop.nix
-            ./home/system-config
-            home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
+                specialArgs = { inherit inputs; super = self; hostname = "laptop"; } // passOn;
+                modules = [
+                # Import the previous configuration.nix we used,
+                # so the old configuration file still takes effect
+                ./system/hosts/laptop.nix
+                ./home/system-config
+                home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
 
-                home-manager.users.hrssilva = import ./home;
-                home-manager.extraSpecialArgs = { super = self; };
-                home-manager.backupFileExtension = "backup";
-                # Optionally, use home-manager.extraSpecialArgs to pass
-                # arguments to home.nix
-              }
-            ];
+                    home-manager.users.hrssilva = import ./home;
+                    home-manager.extraSpecialArgs = { super = self; };
+                    home-manager.backupFileExtension = "backup";
+                    # Optionally, use home-manager.extraSpecialArgs to pass
+                    # arguments to home.nix
+                  }
+                ];
+            };
         };
-      };
-  };
+    };
 }
