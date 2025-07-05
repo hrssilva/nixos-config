113e79e52f2fc008474a2e8dfe30e825bbf5d25a
hrssilva <haroldo.rssilva@gmail.com>
2025-07-05 04:07:00 -0300

    Start bash in vi mode

--- home/hrssilva/default.nix
+++ home/hrssilva/default.nix
@@ -94,6 +94,7 @@
     enableCompletion = true;
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+      set -o vi
     '';
 
     # set some aliases, feel free to add more or remove some
