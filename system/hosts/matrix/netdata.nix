33a38bbc32f79b033c1178b8bd73b0c00af9593c
hrssilva <haroldo.rssilva@gmail.com>
2025-07-12 17:50:20 -0300

    Still trying to make netdata work

--- system/hosts/matrix/netdata.nix
+++ system/hosts/matrix/netdata.nix
@@ -1,27 +1,30 @@
 {pkgs, ...}:
 
 {
-    services.netdata = {
-        enable = true;
-        config = {
-            global = {
-                "memory mode" = "ram";     
-                "update every" = "1";
-                "web files owner" = "netdata";  
-                "web files group" = "netdata";  
-                "web directory" = "${pkgs.netdata}/share/netdata/web";
-            };
-            web = {
-                "bind to" = "localhost:19999 *:19999";
-                "enable ssl" = "no";  
-            };
-        };
+  services.netdata = {
+    enable = true;
+
+    config = {
+      global = {
+        "memory mode" = "save";
+        "update every" = "1";
+        "web files owner" = "netdata";
+        "web files group" = "netdata";
+        "web directory" = "${pkgs.netdata}/share/netdata/web";
+      };
+
+      web = {
+        "default port" = "19999";
+        "bind to" = "localhost:19999 *:19999";
+        "enable web responses gzip" = "yes";
+        "enable dashboard" = "yes";  
+        "serve dashboard" = "yes";   
+        "enable ssl" = "no";
+      };
     };
+  };
 
-    networking.firewall.allowedTCPPorts = [ 19999 ]; 
+  networking.firewall.allowedTCPPorts = [ 19999 ];
 
-    environment.systemPackages = with pkgs; [
-        netdata
-    ];
 }
 
