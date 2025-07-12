ec3c3bcf1a4323b5d71f6affe075dcb977bc89d9
hrssilva <haroldo.rssilva@gmail.com>
2025-07-12 13:41:56 -0300

    One more path fix

--- home/hrssilva/hosts/laptop/hypr/default.nix
+++ home/hrssilva/hosts/laptop/hypr/default.nix
@@ -1,5 +1,5 @@
-{ config, pkgs, super, ... }: let 
-    dotPath = "${config.home.homeDirectory}/nixos-config/home/${config.home.username}/hypr/scripts";
+{ config, pkgs, super, hostname, ... }: let 
+    dotPath = "${config.home.homeDirectory}/nixos-config/home/${config.home.username}/hosts/${hostname}/hypr/scripts";
 in {
     xdg.configFile."hypr/scripts".source = config.lib.file.mkOutOfStoreSymlink dotPath;
 
