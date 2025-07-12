a214186f48b5ac882675f38d985c85f21463c38c
hrssilva <haroldo.rssilva@gmail.com>
2025-07-12 02:16:37 -0300

    fixed wrong name for mdadm

--- system/hosts/matrix/default.nix
+++ system/hosts/matrix/default.nix
@@ -15,7 +15,13 @@
       #../modules/displaymanagers/hyprland.nix
     ];
 
-  boot.initrd.mdadm.enable = true;
+  boot.initrd.swraid.enable = true;
+  
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
 
   environment.systemPackages = with pkgs; [
     mdadm  # RAID
