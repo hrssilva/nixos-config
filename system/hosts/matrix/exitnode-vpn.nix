266136b8a71536be622c4a9cb4a5b251f1fafde9
hrssilva <haroldo.rssilva@gmail.com>
2025-07-14 17:29:48 -0300

    Changed pure secrets to env files

--- system/hosts/matrix/exitnode-vpn.nix
+++ system/hosts/matrix/exitnode-vpn.nix
@@ -1,18 +1,7 @@
 { pkgs, config, lib, ... }:
-let
-    # Using templates to generate with agenix on activation script
-    v-tailscale-key = "@v-tailscale-key@";
-    v-vpn-password = "@v-vpn-password@";
-    v-vpn-username = "@v-vpn-username@";
-
-in {
+{
     virtualisation.docker.enable = true;
 
-    system.activationScripts."exitnode-secret" = lib.stringAfter [ "etc" "agenix" ] ''
-        v-tailscale-key=$(cat "${config.age.secrets.tailscale.path}")
-        v-vpn-password=$(cat "${config.age.secrets.vpn-password.path}")
-        v-vpn-username=$(cat "${config.age.secrets.vpn-user.path}")
-    '';
 
     virtualisation.oci-containers.containers = {
         gluetun = {
@@ -25,11 +14,13 @@ in {
             ];
             environment = {
                 VPN_SERVICE_PROVIDER = "protonvpn";
-                OPENVPN_USER = v-vpn-username;
-                OPENVPN_PASSWORD = v-vpn-password;
                 TZ = "America/Sao_Paulo";
                 UPDATER_PERIOD = "24h";
             };
+            environmentFiles = [
+                config.age.secrets.vpn-password.path
+                config.age.secrets.vpn-user.path
+            ];
             volumes = [
                 "/var/lib/exitnode-vpn/gluetun:/gluetun"
             ];
@@ -48,8 +39,10 @@ in {
                 TS_HOSTNAME = "vpn-exit-node";
                 TS_EXTRA_ARGS = "--advertise-exit-node";
                 TS_STATE_DIR = "/state";
-                TS_AUTHKEY = v-tailscale-key;
             };
+            environmentFiles = [
+                config.age.secrets.tailscale.path
+            ];
             volumes = [
                 "/var/lib/exitnode-vpn/tailscale/var/lib:/var/lib"
                 "/var/lib/exitnode-vpn/tailscale/state:/state"
