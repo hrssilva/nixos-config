9ce2230fd77af4da1f9e7e382dca9b0ae08002aa
hrssilva <haroldo.rssilva@gmail.com>
2025-07-16 19:42:55 -0300

    Typo on config name

--- system/hosts/matrix/secrets.nix
+++ system/hosts/matrix/secrets.nix
@@ -4,7 +4,7 @@
         tailscale.file = ../../../secrets/exitnode-tailscale.age;
         vpn-env.file = ../../../secrets/exitnode-vpn-env.age;
         nextcloud-password.file = ../../../secrets/nextcloud-password.age;
-        config.age.secrets.cloudflared.file = ../../../secrets/cloudflared.age;
-        config.age.secrets.cloudflared-home-server.file = ../../../secrets/cloudflared-home-server.age;
+        cloudflared.file = ../../../secrets/cloudflared.age;
+        cloudflared-home-server.file = ../../../secrets/cloudflared-home-server.age;
     };
 }
